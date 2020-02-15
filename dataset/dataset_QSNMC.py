c167bf130a08ce9af1be17ff1432f55d57b79b84
Peter Svenningsson <peter.o.svenningsson@gmail.com>
2020-02-15 20:51:26 +0100

    Added the model class. Added similarity measure to script.

---
+++ dataset/dataset_QSNMC.py
@@ -0,0 +1,104 @@
+""" Dataset loader for QSNMC 2009a dataset."""
+###########
+# IMPORTS #
+###########
+# Standard
+import os
+import pickle
+
+# Third party
+import numpy as np
+from NeuroTools.signals import AnalogSignal
+import matplotlib.pyplot as plt
+
+#############
+# CONSTANTS #
+#############
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+#############
+# CLASSES #
+#############
+
+class QSNMC:
+    """ Dataset loader for data belonging the 2009 competition QSNMC.
+    """
+    def __init__(self, file_current = PATH + '/2009a/current.txt', file_voltage = PATH + '/2009a/voltage_allrep.txt', dt = 1e-4):
+        self.file_current = file_current
+        self.file_voltage = file_voltage
+        self.dt = 1e-4 # 10 kHz sampling.
+        self.current = None
+        self._current = None
+        self.voltage = None
+        self.spike_trains = []
+        self.gold_voltages = []
+        self.initialize_dataset()
+
+    def _load_dataset(self):
+        """ Loads the raw dataset voltage and current.
+        """
+        print("Loading data from .txt...")
+        current = np.loadtxt(self.file_current)
+        voltage = np.loadtxt(self.file_voltage)
+
+        # trunkate the current signal to the length of the voltage signal.
+        current = current[0:voltage.shape[0]]
+        self._current = current
+        self.voltage = voltage
+
+    def pickle_dataset(self):
+        """ Serializes the dataset for faster load time using Pickle.
+        """
+        self._load_dataset()
+        print("Serializing dataset...")
+        current_pickle_path = self.file_current + '.pickle'
+        voltage_pickle_path = self.file_voltage + '.pickle'
+        with open(current_pickle_path, 'wb') as f:
+            pickle.dump(self._current, f)
+        with open(voltage_pickle_path, 'wb') as f:
+            pickle.dump(self.voltage, f)
+
+    def load_pickled_dataset(self):
+        """ Loads serialized dataset from .pickle file.
+        """
+        current_pickle_path = self.file_current + '.pickle'
+        voltage_pickle_path = self.file_voltage + '.pickle'
+
+        try:
+            print("Attempting to read dataset from pickled files")
+            with open(current_pickle_path, 'rb') as f:
+                current = pickle.load(f)
+            with open(voltage_pickle_path, 'rb') as f:
+                voltage = pickle.load(f)
+        except FileNotFoundError:
+            print("Serialized data not found...")
+            self.pickle_dataset()
+            with open(current_pickle_path, 'rb') as f:
+                current = pickle.load(f)
+            with open(voltage_pickle_path, 'rb') as f:
+                voltage = pickle.load(f)
+        self._current = current
+        self.voltage = voltage
+
+    def _generate_spike_trains(self):
+        """ Calculates spike trains from the voltage data. Spike threshold set to 0.
+        """
+        print("Generating dataset spike trains.")
+        for col in range(self.voltage.shape[1]):
+            voltage_trial = self.voltage[:, col]
+            self.gold_voltages.append(voltage_trial)
+
+            vm_trial = AnalogSignal(voltage_trial, self.dt)
+            spike_train = vm_trial.threshold_detection(0)
+            self.spike_trains.append(spike_train)
+        self.current = AnalogSignal(self._current, self.dt)
+
+    def initialize_dataset(self):
+        """ Initializes the dataset by setting instance variables.
+        """
+        self.load_pickled_dataset()
+        self._generate_spike_trains()
+
+##########
+# SCRIPT #
+##########
--- models/iz_neuron.py
+++ models/iz_neuron.py
@@ -8,19 +8,12 @@ import sciunit
 import numpy as np
 from neuronunit.capabilities import ProducesSpikes
 from NeuroTools.signals import AnalogSignal
-import matplotlib.pyplot as plt
 
 
 #############
 # CONSTANTS #
 #############
-TMAX = 39.0
-#TMAX = None
-a = 0.02
-b = 0.2
-c = -50
-d = 2
-v_spike = 30
+
 
 # TODO: Implement peak finding to set correct value v_spike.
 
@@ -29,31 +22,27 @@ class IZModel(sciunit.Model, ProducesSpikes):
     Model of a Izhikevich neuron. Extends sciunit.Model and interfaces neuronunit.capabilities.ProducesSpikes.
     """
 
-    def __init__(self, a, b, c, d, v_spike=30.0, **kwargs):
+    def __init__(self, a, b, c, d, v_spike=30.0, TMAX = 39.0, **kwargs):
         self.a = a  # Recovery parameter for variable u. Low a provides long recovery.
-        self.b = b  # Sensativity of u to subthreshhold dynamics of v.
+        self.b = b  # Sensativity of u to sub-threshhold dynamics of v.
         self.c = c  # Reset value of potential v after action potential spike.
         self.d = d  # Reset value of potential u after action potential spike.
         self.v_spike = v_spike
+        self.TMAX = TMAX
         self.u = None # Initialized in simulate method.
         self.v = None # Initialized in simulate method.
         self.dt = None # Initialized by set_external_current method.
         self.i_ext = None # Initialized by set_external_current method.
         self.dt = None # Initialized by set_external_current method.
-        self.steps = None # Initialized by set_external_current method.
 
         super().__init__(**kwargs)
 
     def set_external_current(self, current):
         """ Parsing function for the NeuroTools AnalogSignal type object current.
         """
-        assert type(current) is AnalogSignal, \
-            "Current should be a NeuroTools AnalogSignal, not a %s" % type(current)
-        print("Simulating current injection...")
 
         self.i_ext = current.signal
         self.dt = current.dt
-        self.steps = len(self.i_ext)
 
     def euler_forward(self, u, v, I):
         """ Calculates one Euler step for the Izhikevich neuron.
@@ -63,7 +52,8 @@ class IZModel(sciunit.Model, ProducesSpikes):
         Returns:
             _v - the proceeding voltage
             _u - the proceeding recovery
-            """
+        """
+
         # The constants 0.04, 5, 140 are the one fits all parameters from Simple Model of Spiking Neurons E. Izhikevich.
         # These constants are justified when simulating large networks of neurons.
         # TODO: Parameterise the four constants 0.04, 5, 140.
@@ -71,16 +61,13 @@ class IZModel(sciunit.Model, ProducesSpikes):
         _u = u + self.dt * (self.a * (self.b * v - u))
         return _v, _u
 
-    def simulate(self, T_max=None):
+    def simulate(self):
         """ Simulates the pulse train of the Izhikevich neuron by setting self.v_model.
         Inputs:
             T_max - Maximum simulation time in ms
         Outputs:
         """
-        if T_max is None:
-            steps = self.steps
-        else:
-            steps = int(T_max / self.dt)
+        steps = int(self.TMAX / self.dt)
 
         # Initialize variables
         self.u = np.zeros(steps)
@@ -105,26 +92,28 @@ class IZModel(sciunit.Model, ProducesSpikes):
 
 
 
-    def get_spike_trains(self, current=None):
+    def get_spike_train(self, current):
         """ Calculates the spike train produced by the external current.
         Inputs:
             current - AnalogSignal type instance describing the external current.
         Outputs:
-
         """
 
-        # For compability with sciunit as many spike trains are generated as there exists ground truth observations
         spike_trains = []
-        if current:
-            self.set_external_current(current)
-        self.simulate(T_max=TMAX)
+        self.set_external_current(current)
+        self.simulate()
         voltage_trial = self.v
         vm_trial = AnalogSignal(voltage_trial, self.dt)
         spike_train = vm_trial.threshold_detection(0)
-        spike_trains = [spike_train for _ in range(0,3)]
-        return spike_trains
+        return spike_train
 
 
 # Instantiate a model.
 
-iz_model = IZModel(a, b, c, d, v_spike, name='Izhikevich')
+#a = 0.02
+#b = 0.2
+#c = -50
+#d = 2
+#v_spike = 30
+
+#iz_model = IZModel(a, b, c, d, v_spike, name='Izhikevich')
